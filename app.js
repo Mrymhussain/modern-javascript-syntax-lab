6507b5a04831638ffd9def3d041b84f1b84f4761
Maryam Hussain <202011514@stu.uob.edu.bh>
2026-08-30 10:34:18 +0300

    ex4 is completed

--- app.js
+++ app.js
@@ -1,13 +1,4 @@
 // ! Exercise 1:
-// a. Use the `map()` method to iterate over the provided `nums` array
-//
-// b. Use the callback function of the `map()` method you wrote to
-//    create a new array where each value is multiplied by 2.
-//    Name the new array `numsTimesTwo`.
-//
-// c. Console log the new array.
-//
-// Starting code (don't modify this):
 
 const nums = [13, 87, 2, 89, 12, 4, 90, 63];
 
@@ -18,14 +9,6 @@ const numsTimesTwo = nums.map((num) => {
 console.log(numsTimesTwo);
 
 // ! Exercise 2:
-// a. Given the provided `pizzaToppings` array, use destructuring to extract 
-//    the first and second values and place them into variables. Name the 
-//    variable that corresponds to the first value `firstIngredient`. Name the 
-//    variable that corresponds to the second value `secondIngredient`.
-//
-// b. Console log the `firstIngredient` and `secondIngredient` variables.
-//
-// Starting code (don't modify this):
 
 const pizzaToppings = ['Pineapple', 'Olives', 'Anchovies'];
 
@@ -36,12 +19,6 @@ console.log(secondIngredient);
 
 
 // ! Exercise 3:
-// a. Given the provided `car` object, use destructuring to create two
-//    variables: `make` and `model` that will hold the respective values.
-//
-// b. Console log the `make` and `model` variables.
-//
-// Starting code (don't modify this):
 
 const car = {
     make: 'Audi',
@@ -51,3 +28,11 @@ const car = {
 
 console.log(make);
 console.log(model);
+
+// ! Exercise 4:
+
+const morePizzaToppings = ['Cheese', 'Sauce'];
+
+const uncontroversialPizzaToppings = [...morePizzaToppings];
+
+console.log(uncontroversialPizzaToppings);
